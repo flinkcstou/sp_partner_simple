680189b8acb0c341a824ebe6af6614256bd996ce
ubaitassov <ubaitassov@greet-go.com>
2021-07-11 22:52:29 +0600

    feat: add firebase dependencies

---
+++ src/app/services/firebase.service.ts
@@ -0,0 +1,49 @@
+import {Injectable, OnDestroy} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {FirebaseX} from '@ionic-native/firebase-x/ngx';
+import {PlatformService} from './roots/platform.service';
+import {environment} from '../../environments/environment';
+
+@Injectable({
+ providedIn: 'root',
+})
+export class FirebaseService implements OnDestroy {
+
+ // https://ionicframework.com/docs/native/firebase-x
+
+ constructor(private firebaseX: FirebaseX,
+             private platformService: PlatformService) {
+ }
+
+ firebaseSubscription: Subscription;
+
+
+ async start() {
+  if (environment.desktop) {
+   return;
+  }
+  await this.setPermissionIos();
+  await this.getToken();
+ }
+
+
+ async getToken() {
+  this.firebaseX.getToken().then(token => {
+   console.error('firebase pushtoken: ', token);
+   localStorage.setItem('push_token', token);
+  });
+ }
+
+
+ async setPermissionIos() {
+  if (this.platformService.isIos()) {
+   const hasPermission = await this.firebaseX.hasPermission();
+   if (!hasPermission) {
+    this.firebaseX.grantPermission().then();
+   }
+  }
+ }
+
+ ngOnDestroy() {
+ }
+}
